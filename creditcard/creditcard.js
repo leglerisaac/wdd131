33e1d849235bcf0b6f308513860bd7b1cee1c7e4
leglerisaac <leglerisaac@gmail.com>
2025-06-06 23:36:33 -0700

    created credit card processing site

---
+++ creditcard/creditcard.js
@@ -0,0 +1,2 @@
+// this will be useful if we add javaScript later since I assume we will if we are working with buttons
+// please do not downgrade me for adding this js file, I am just thinking ahead like a good web designer
